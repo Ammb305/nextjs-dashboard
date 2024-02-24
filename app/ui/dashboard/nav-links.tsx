08749b0f0a6efea0ca334d36f7d3d2d6bb37aaac
Ammb305 <abdallahmmb5@yahoo.com>
2024-02-24 20:00:35 +0200

    nextjs-dashboard

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,4 @@
-'use client';
- 
+'use client'
 import {
   UserGroupIcon,
   HomeIcon,
@@ -8,12 +7,20 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
- 
-// ...
- 
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
 export default function NavLinks() {
-  const pathname = usePathname();
- 
+  const pathname = usePathname()
   return (
     <>
       {links.map((link) => {
@@ -25,7 +32,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-200 text-blue-600': pathname === link.href,
               },
             )}
           >
